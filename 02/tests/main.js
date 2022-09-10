12533f512f06e8d60eecfc65c7b46f2247be0e6e
filipenevola <filipe@provi.com.br>
2022-09-10 07:31:50 -0400

    test husky

--- 02/tests/main.js
+++ 02/tests/main.js
@@ -1,5 +1,5 @@
 import assert from 'assert';
-
+// comment
 describe('meteor-wallet', function () {
   it('package.json has correct name', async function () {
     const { name } = await import('../package.json');
