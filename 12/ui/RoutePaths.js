ba35bfd98c9f3e5988ca7839b6defdf4d5d20f42
filipenevola <filipe@provi.com.br>
2022-09-14 21:32:16 -0400

    - Installed quave:accounts-passwordless-react - Replaced Access component's content by <Passwordless /> component. - Removed the other routes like ForgotPassword and ResetPassword.

--- 12/ui/RoutePaths.js
+++ 12/ui/RoutePaths.js
@@ -1,7 +1,5 @@
 export const RoutePaths = {
   HOME: '/',
   ACCESS: '/access',
-  FORGOT_PASSWORD: '/forgot-password',
-  RESET_PASSWORD: '/reset-password',
   REMOVE_TRANSACTION: '/remove-transaction',
 };
